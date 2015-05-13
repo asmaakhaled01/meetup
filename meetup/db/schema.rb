793f21eebb91c0e1b3a69207f0b7484842dccd4d
yomna <yomnakhaled0@gmail.com>
2015-05-13 16:18:20 +0300

    version 1.0

--- meetup/db/schema.rb
+++ meetup/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512165207) do
+ActiveRecord::Schema.define(version: 20150511160632) do
 
   create_table "attendens", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
